c137fd7df9c35908c261a5b996e2fb4abb7380ed
Pashkiin <rtoit007pele@gmail.com>
2025-05-10 22:00:13 +0200

    update old models and create new ones nessary for multi-session handling

--- server/src/models/playerModel.js
+++ server/src/models/playerModel.js
@@ -1,13 +1,33 @@
 class PlayerModel {
-    constructor(username) {
-        this.id = Math.random().toString(36).substring(7);
-        this.username = username;
-        this.selectedCard = null;
+  constructor(username, socketId = null) {
+    if (!username) {
+      throw new Error("Username is required for a player.");
     }
+    this.id = Math.random().toString(36).substring(2, 9);
+    this.username = username;
+    this.socketId = socketId;
 
-    selectCard(card) {
-        this.selectedCard = card;
-    }
+    // selectedCard will store the vote for the current task
+    this.selectedCardValue = null;
+    this.hasLockedVote = false;
+  }
+
+  selectCard(card) {
+    this.selectedCardValue = card;
+  }
+
+  setVoteLock(locked) {
+    this.hasLockedVote = locked;
+  }
+
+  resetVote() {
+    this.selectedCardValue = null;
+    this.hasLockedVote = false;
+  }
+
+  updateSocketId(socketId) {
+    this.socketId = socketId;
+  }
 }
 
 module.exports = { PlayerModel };
